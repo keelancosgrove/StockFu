a2cde3c97735c762c5fc3da3b9e7f7e6af082f5a
Louis Cruz <cruzlouis13@gmail.com>
2016-04-30 20:18:21 -0400

    Gather user charts from DB and display them in test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 session_start();
-if (!isset($_SESSION['logged_user'])){
-    header('Location: StockFuLogin.php');
+if (!isset($_SESSION['logged_user'])) {
+    //    header('Location: StockFuLogin.php');
 }
 ?>
 
@@ -9,7 +9,7 @@ if (!isset($_SESSION['logged_user'])){
 <html>
 
 <head>
-    <meta charset="UTF-8" />    
+    <meta charset="UTF-8" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -77,87 +77,72 @@ if (!isset($_SESSION['logged_user'])){
 </style>
 
 <body>
-    <?php include 'navbar.php'; ?>
-    <div class="container">
-        <div class="row">
-            <h1 class="page-title">Your Charts<h1>
-        </div>
-        <div class="row">
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="symbol">AAPL</h1>
-                    <h4 class="company">Apple Inc.</h4>
-                    <p class="dates">Jan. 4, 2000 - Present</p>
-                </a>
-            </div>
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="symbol">GOOG</h1>
-                    <h4 class="company">Alphabet Inc.</h4>
-                    <p class="dates">Aug. 19, 2004 - Aug. 19, 2014</p>
-                </a>
-            </div> 
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="symbol">CMCSA</h1>
-                    <h4 class="company">Comcast Corp.</h4>
-                    <p class="dates">Aug. 19, 2004 - Aug. 19, 2014</p>
-                </a>
-            </div>  
-        </div>
-        <div class="row">
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="symbol">DJIA</h1>
-                    <h4 class="company">Dow Jones Industrial Avg.</h4>
-                    <h5 class="name">How well my portfolio is going</h5>
-                    <p class="dates">This Week</p>
-                </a>
-            </div>
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="multi-symbol">CMCSA & GOOG</h1>
-                    <h4 class="company">Comcast Corp. & Alphabet Inc.</h4>
-                    <p class="dates">Aug. 19, 2004 - Aug. 19, 2014</p>
-                </a>
-            </div> 
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="symbol">FB</h1>
-                    <h4 class="company">Facebook Inc.</h4>
-                    <p class="dates">Max</p>
-                </a>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="symbol">NASDAQ</h1>
-                    <h4 class="company">Nasdaq Inc.</h4>
-                    <p class="dates">This Week</p>
-                </a>
-            </div>
-            <div class="col-md-4" id="stock">
-                <a href="viewChartPrivate.php">
-                    <h1 class="multi-symbol">F & GM</h1>
-                    <h4 class="company">Ford Motor Co. & General Motors Co.</h4>
-                    <p class="dates">Aug. 19, 2004 - Aug. 19, 2014</p>
-                </a>
-            </div>
-            <div class="col-md-4">
-                <a href="makeNew.php">
-                    <h1 id="plus-sign">+</h1>
-                </a>
-            </div>
-        </div>  
-    </div>
+    <?php
+include 'navbar.php';
+?>
+
+    <?php
+
+/* Display all user charts */
+
+if (isset($GET['userID'])) {
+    require_once 'config.php';
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+
+    /*Check if userID = logged_user and display their charts*/
+    $query  = "SELECT userID FROM Users WHERE username = $_SESSION['logged_user']";
+    $result = $mysqli->query($query);
+
+    if ($result) {
+        /*current user = userID*/
+        $query  = "SELECT * FROM Charts WHERE userID = $GET[userID]";
+        $result = $mysqli->query($query);
+        if ($result) {
+            /*user has charts, display them*/
+            echo "<div class=\"container\">
+                  <div class=\"row\">
+                      <h1 class=\"page-title\">Your Charts<h1>
+                  </div>";
+
+            $row   = $result->fetch_assoc();
+            $count = 0;
+
+            while ($row) {
+              echo "<div class=\"row\">";
+                while ($row && $count != 3) {
+                  echo "<div class=\"col-md-4\" id=\"stock\">
+                      <a href=\"viewChartPrivate.php/?chartID=$row[chartID]\">
+                          <h1 class=\"symbol\">$row['symbol']</h1>
+                          <h4 class=\"company\">$row['company']</h4>
+                          <p class=\"dates\">$row['startDate'] - $row['endDate']</p>
+                      </a>
+                  </div>";
+                  $count++;
+                }
+              echo "</div>";
+              $count = 0;
+            }
+          echo "<div class="col-md-4">
+                  <a href="makeNew.php">
+                  <h1 id="plus-sign">+</h1>
+                  </a>
+                </div>";
+          echo "</div>";
+        }
+    }
+    /*If userID != logged_user, do something else*/
+    //TODO: Display 404 page or access denied page
+}
+
+?>
 
     <footer>
         <!-- Tell people that this is my website do not steal -->
         <div id="copyright">
             Copyright &copy; 2016 The Web Development Group. All rights reserved.
         </div>
-    </footer> 
-    
+    </footer>
+
 </body>
 </html>
