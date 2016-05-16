a41c9edbacf30e620985d603bb951eed75d935e8
SirDehavillandOfComet <kg344@cornell.edu>
2016-05-16 15:56:06 -0400

    Removed some whitespace and changed the chart
    Put text in the About page into a grey box, changed the chart on that
    page into one that was useable

--- About.php
+++ About.php
@@ -41,35 +41,36 @@ session_start();
             include 'altNavBar.php';
         }
     ?>
-
-    <div class="row">
-        <div>
-            <h1 class="page-title">What is StockFu?</h1>
+        <div class="row">
+            <div>
+                <h1 class="page-title">What is StockFu?</h1>
+            </div>
+        </div>
+    <div id="blockText">
+        <div class="row">
+            <div class="col-md-12" id="body-text">
+                <p>StockFu allows anyone to explore the stock market in an accessible, customizable, and interactive way. The idea is to let you make your own profile of stock charts, which could vary on company, date range, and more. You can create and customize these charts, and re-visit them later on. You can also compare and contrast companies on one stock chart, or with the same company across different date ranges. Whether you're a beginner or expert, student or professional, you can get started and explore the world of stocks!</p> <br>
+                <p>StockFu was created by Andrew Cadwallader in 2016. He sought to integrate web technologies with the world of stocks in order to give his audience, primarily college students, a way to easily customize and explore stock charts. </p>
+                <p>Quandl.com is used to retrieve stock data.</p>
+            </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
-            <p>StockFu allows anyone to explore the stock market in an accessible, customizable, and interactive way. The idea is to let you make your own profile of stock charts, which could vary on company, date range, and more. You can create and customize these charts, and re-visit them later on. You can also compare and contrast companies on one stock chart, or with the same company across different date ranges. Whether you're a beginner or expert, student or professional, you can get started and explore the world of stocks!</p> <br>
-            <p>StockFu was created by Andrew Cadwallader in 2016. He sought to integrate web technologies with the world of stocks in order to give his audience, primarily college students, a way to easily customize and explore stock charts. </p>
-            <p>Quandl.com is used to retrieve stock data.</p>
+        <div class="row">
+            <div class="col-md-12" id="body-text">
+                <h3>An example chart is shown below, for Yahoo's stock price data from 2013 to 2016</h3>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <h3>An example chart is shown below, for Yelp data from 2013 to 2016</h3>
+        <div class="row">
+            <div class="col-md-12" id="body-text">
+            <?php
+              require_once("config.php");
+              $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+              $getChart = $mysqli -> query("SELECT svg_string FROM Charts WHERE chartID = 40");
+              $exampleChart = $getChart -> fetch_assoc();
+              print($exampleChart['svg_string']);
+            ?>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-        <?php
-          require_once("config.php");
-          $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-          $getChart = $mysqli -> query("SELECT svg_string FROM Charts WHERE chartID = 1");
-          $exampleChart = $getChart -> fetch_assoc();
-          print($exampleChart['svg_string']);
-        ?>
-        </div>
-    </div>
         <div id="footer">
             <footer>
                     Copyright &copy; 2016 The Web Development Group. All rights reserved.
